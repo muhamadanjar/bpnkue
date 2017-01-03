e71f537d2a0e464500295f8b69cf421d41e8a0ce
Muhamad anjar <arvanria@gmail.com>
2017-01-03 15:34:01 +0700

    initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        // Basic roles data
+        App\Role::insert([
+            ['name' => 'admin'],
+            ['name' => 'manager'],
+            ['name' => 'editor'],
+        ]);
+    
+        // Basic permissions data
+        App\Permission::insert([
+            ['name' => 'access.backend'],
+            ['name' => 'create.user'],
+            ['name' => 'edit.user'],
+            ['name' => 'delete.user'],
+            ['name' => 'create.article'],
+            ['name' => 'edit.article'],
+            ['name' => 'delete.article'],
+        ]);
+    
+        // Add a permission to a role
+        $role = App\Role::where('name', 'admin')->first();
+        $role->addPermission('access.backend');
+        $role->addPermission('create.user');
+        $role->addPermission('edit.user');    
+        $role->addPermission('delete.user');
+        // ... Add other role permission if necessary
+    
+        // Create a user, and give roles
+        $user = App\User::create([
+            'email' => 'admin@example.com',
+            'name' => 'admin',
+            'password' => bcrypt('password'),
+        ]);
+    
+        $user->assignRole('admin');
+    }
+}
